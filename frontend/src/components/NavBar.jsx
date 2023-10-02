f2ab2d3863c0c9d080cb56f57844cac7eabc07d2
shreyanzjain <shreyanz32@gmail.com>
2023-10-02 10:24:07 +0530

    refactor nabvbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-
 function NavBar() {
   return (
-    <div className='container w-full bg-teal-900 h-16 sticky p-0 m-0'>
-        <div className='flex h-full items-center justify-between'>
-            <div className='container ms-6 text-white font-bold'>
-                Guten-Morgen
-            </div>
-            <div className='container flex items-center justify-end me-6 text-white font-bold'>
-                <p className='me-4'>Register</p>
-                <p>Login</p>
-            </div>
+    <div className="mx-auto w-full h-1/6 bg-slate-300">
+      <div className="mx-auto w-full bg-teal-900 h-16 fixed">
+        <div className="flex h-full items-center justify-between text-slate-50">
+          <div className="ms-6 me-6 font-bold">Guten-Morgen</div>
+          <div className="flex items-center justify-end me-6 font-bold">
+            <a className="ms-6 me-6" href="#">
+              Register
+            </a>
+            <a href="#">Login</a>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
