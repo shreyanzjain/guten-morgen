01626e75485cf918f80638705c73709e2b1c375d
shreyanzjain <shreyanz32@gmail.com>
2023-11-16 19:28:38 +0530

    better loading screen

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,10 +1,14 @@
+import { CircularProgress } from '@mui/material';
+
 function Loading() {
   return (
     <div className="h-screen bg-slate-300">
       <div className="flex h-4/6 items-start justify-center">
         <div className="h-full w-3/4 md:w-1/2 lg:w-1/3 rounded-lg bg-slate-50 shadow-md mt-32">
             <div className="flex h-full items-center justify-center">
-                <div className="text-2xl">Loading........</div>
+                <div className="text-2xl text-black">
+                  <CircularProgress color='inherit'/>
+                </div>
             </div>
         </div>
       </div>
